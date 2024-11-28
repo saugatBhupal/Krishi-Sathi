bfc96643f7a79d0e6dd6b5a9a881dd3b8cd4780b
Saugat Singh <saugatbhupal@bhunet.com>
2024-11-28 13:01:24 +0545

    first request sucess

--- api/routes/routes.py
+++ api/routes/routes.py
@@ -1,6 +1,6 @@
-from flask import Flask 
+from flask import Flask, request, jsonify, send_file
 
-app ==Flask(__name__)
+app =Flask(__name__)
 
 def upload_image():
     pass
@@ -8,5 +8,12 @@ def upload_image():
 def answer_question():
     pass
 
+@app.route('/ask-bot', methods=['POST'])
 def get_tts():
-    pass
+    Text = request.get_json()
+    q = Text.get('text', None)
+    print(q)
+    return("hello")
+
+if(__name__ == "__main__"):
+    app.run(debug=True, port = 5000)
